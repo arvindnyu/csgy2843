9ba242ca23d3d406af642e84bdb05d4cb607a7c3
Arvind Namasivayam <an3391@nyu.edu>
2021-02-23 23:55:49 -0500

    solution for assignment 03 debug = false

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import assignment02.solution
+import assignment03.solution
 
 if __name__ == "__main__":
-    assignment02.solution.webServer(13331)
+    #assignment03.solution.smtp_client(25,'smtp.nyu.edu')
+    assignment03.solution.smtp_client(1025,'localhost')
